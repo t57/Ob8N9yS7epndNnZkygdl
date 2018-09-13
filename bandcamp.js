aa6f51eecc5fe716477f75cfbb4356e46ff47209
t57 <4317605+t57@users.noreply.github.com>
2018-09-12 20:04:07 -0500

    Update bandcamp.js

--- bandcamp.js
+++ bandcamp.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bandcamp Floating Player
 // @namespace
-// @version      0.1
+// @version      0.2
 // @description
 // @author       t57
 // @match        https://*.businesscasual.biz/album/*
@@ -12,7 +12,7 @@
 (function() {
     'use strict';
 
-    // [1] [5]
+    // [1]
     function findBgColor() {
         var sheets = document.styleSheets;
         for (var i = 0; i < sheets.length; i++) {
@@ -27,29 +27,28 @@
         return "rgb(256, 256, 256)";
     }
 
-    // [2] [6]
-    function adjustColor(color) {
-        //function adjust(x) { return x < Math.sqrt(128) ? x + 2 : x - 2; }
-        //function adjust2(x) { return x < 128 ? x + 4 : x - 4; }
-        function adjust3(x) { return x - Math.floor((x-128)/5); }
-        //function square(x) { return x * x; }
+    // [2]
+    function shiftColor(color) {
+        var numbersStart = color.indexOf("(") + 1;
+        var numbersEnd = color.indexOf(")");
+        var numbers = color.substr(numbersStart, numbersEnd - numbersStart);
 
-        var rgbArray = Function("rgb=Array;return "+color)();
-        //rgbArray = rgbArray.map(Math.sqrt).map(adjust).map(square).map(Math.floor);
-        rgbArray = rgbArray.map(adjust3);
+        var rgbArray = Function("return ["+numbers+"]")();
+        
+        function shift(x) { return x - Math.floor((x-128)/5); }
+        rgbArray = rgbArray.map(shift);
 
         return "rgb("+rgbArray[0]+", "+rgbArray[1]+", "+rgbArray[2]+")";
     }
 
-    // [3]
     var bgColor = findBgColor();
-    var adjustedColor = adjustColor(bgColor);
+    var newColor = shiftColor(bgColor);
 
     var player = document.getElementsByClassName("inline_player")[0];
 
     var style = {
-        backgroundColor: adjustedColor,
-        border: "20px solid "+adjustedColor,
+        backgroundColor: newColor,
+        border: "20px solid "+newColor,
         borderRadius: "5px",
         position: "fixed",
         bottom: "0px",
@@ -58,15 +57,12 @@
         maxWidth: "500px",
     }
 
-    // [4]
+    // [3]
     for (var property in style) player.style[property] = style[property];
 
     /** Notes:
     1. https://stackoverflow.com/questions/324486/how-do-you-read-css-rule-values-with-javascript/27527462#27527462
-    2. I can't find a reference for why I do the sqrt transform, but I saw something about how you're supposed to do that one time.
-    3. #222 also looks nice on black.
-    4. https://stackoverflow.com/questions/8312459/iterate-through-object-properties
-    5. Since I want something that would contrast with white or black, should I return rgb(128, 128, 128) here?
-    6. Consider removing old code.  Also, this needs work when color is #000. Consider eliminating "Array()" call, GM hates it. Manual extract "r, g, b", place in array.
+    2. Make this work with "#00FF00" style colors?
+    3. https://stackoverflow.com/questions/8312459/iterate-through-object-properties
     **/
 })();
